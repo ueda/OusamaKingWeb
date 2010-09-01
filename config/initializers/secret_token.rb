31459fa71e0ac4a3eb8e8a472dab9893224f44ff
Yuki Ueda <ueda@tos.co.jp>
2010-09-01 22:11:29 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OusamakingWeb::Application.config.secret_token = '0fc25275871429d9ae78314aa53de240a599b7203b7abbd9420a70dc4565362b4602d3940454edc30a154f2ff3e634bf0501742b9423843fbfcc841c379ce245'
