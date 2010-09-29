3c155cb202ef3d527131ae82ac23ebf14e941954
植田 結樹 <ueda@MacAirHQDev.local>
2010-09-29 12:43:59 +0900

    changed redirect dest after group creation.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -46,7 +46,8 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to(@group, :notice => 'Group was successfully created.') }
+        #format.html { redirect_to(@group, :notice => 'Group was successfully created.') }
+        format.html { redirect_to(groups_path, :notice => "Group #{@group.name} was successfully updated.") }
         format.xml  { render :xml => @group, :status => :created, :location => @group }
       else
         format.html { render :action => "new" }
