56b4a6b5222bdf0ac6d451def9cfc659053b15ae
Yuki Ueda <ueda@tos.co.jp>
2010-10-03 21:41:09 +0900

    I18n devise view creation & modification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100926140310) do
+ActiveRecord::Schema.define(:version => 20101003050636) do
 
   create_table "friends", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(:version => 20100926140310) do
     t.datetime "scheduled_at"
     t.integer  "created_by"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
